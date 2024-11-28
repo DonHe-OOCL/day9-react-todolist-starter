035673f4abe4720e4d5d5bc7b2d4dcc7832dc4cd
HEDO <hedo@oocl.com>
2024-11-28 20:04:54 +0800

    feat: add todoGenerator

---
+++ src/components/TodoGenerator.jsx
@@ -0,0 +1,29 @@
+import {useState, useContext} from 'react'
+import {TodoContext} from "../App";
+
+
+const TodoGenerator = () => {
+
+    const [text, setText] = useState();
+
+    const {dispatch} = useContext(TodoContext);
+
+    const handleChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleAdd = () => {
+        dispatch({type: 'ADD', payload: text});
+        setText('');
+    }
+
+    return (
+        <div>
+            <input value={text} onChange={handleChange}/>
+            <button onClick={handleAdd}>add</button>
+        </div>
+    );
+
+}
+
+export default TodoGenerator;
