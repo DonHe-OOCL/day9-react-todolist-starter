a1369237fe173a5ffead9a1c58ded5b302d474a6
HEDO <hedo@oocl.com>
2024-11-28 20:21:49 +0800

    feat: click todo text can make it done

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,18 @@
+import {useContext} from "react";
+import {TodoContext} from "../App";
+import "./TodoItem.css";
 
 const TodoItem = ({todo}) => {
 
+    const {dispatch} = useContext(TodoContext)
+
+    const handleDoneTodo = () => {
+        dispatch({type: 'DONE', payload: todo.id});
+    };
+
     return (
         <div>
-            <div>{todo.text}</div>
+            <div className={`todo-text ${todo.done ? "done" : ""}`} onClick={handleDoneTodo}>{todo.text}</div>
         </div>
     );
 }
--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -5,5 +5,11 @@ export const todoReducer = (state, action) => {
   switch (action.type) {
     case 'ADD':
       return [...state, {id: Date.now(), text: action.payload, done: false}];
+    case 'DONE':
+      return state.map((todo) => {
+        return todo.id === action.payload ? {...todo, done: !todo.done} : todo;
+      })
+    default:
+      return state;
   }
 };
