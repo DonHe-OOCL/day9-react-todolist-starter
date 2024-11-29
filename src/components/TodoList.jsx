01cccda6b7c043666fb9e1e1eafe231e4d64e746
HEDO <hedo@oocl.com>
2024-11-29 21:54:35 +0800

    feat: add loading

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,25 +1,37 @@
 import TodoGroup from "./TodoGroup";
 import TodoGenerator from "./TodoGenerator";
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import {TodoContext} from "../context/TodoContext";
 import {getTodoList} from "../api/todo";
 import {INIT} from "../context/todoActions";
+import {Spin} from "antd";
 
 const TodoList = () => {
 
     const {dispatch} = useContext(TodoContext);
 
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         getTodoList().then((todos) => {
             dispatch({type: INIT, payload: todos});
-        })
+        }).finally(() => {
+            setLoading(false);
+        });
     }, []);
 
     return (
         <div>
-            <h2>{"Todo List"}</h2>
-            <TodoGroup/>
-            <TodoGenerator/>
+        {
+            loading ? (<Spin size="large"/>)
+                : (
+                    <div>
+                        <h2>{"Todo List"}</h2>
+                        <TodoGroup/>
+                        <TodoGenerator/>
+                    </div>
+                )
+        }
         </div>
     );
 }
