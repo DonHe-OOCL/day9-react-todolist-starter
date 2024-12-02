399ea1ac1d3e49ce1c61936f19809c3c12c4dd9d
HEDO <hedo@oocl.com>
2024-12-02 15:04:01 +0800

    feat: update interface

--- src/api/todo.js
+++ src/api/todo.js
@@ -2,20 +2,20 @@ import instance from "./interceptor";
 
 export const getTodoList = async () => {
    const response = await instance.get("/todos");
-   return response.data;
+   return response.data.result;
 }
 
 export const addTodoItem = async (todo) => {
    const response = await instance.post("/todos", todo);
-   return response.data;
+   return response.data.result;
 }
 
 export const deleteTodoItem = async (id) => {
    const response = await instance.delete(`/todos/${id}`);
-   return response.data;
+   return response.data.result;
 }
 
 export const updateTodoItem = async (todo) => {
    const response = await instance.put(`/todos`, todo);
-   return response.data;
+   return response.data.result;
 }
